dfce9bdd9a0b1e6032c9d4c00d19a102a16c5565
gottsu52 <19e1810a@shinshu-u.ac.jp>
2021-11-29 10:33:46 +0900

    1st

--- temp.py
+++ temp.py
@@ -10,29 +10,33 @@ from PIL import Image
 import numpy as np
 import cv2
 
-st.title('顔面隠し')
+st.title('顔面潰し')
 
-uploaded_file = st.file_uploader("画像を選択してください", type ='jpg')
+uploaded_file = st.file_uploader("顔面を選択してください", type ='jpg')
 
-left_column, right_column = st.columns(2)
-button = left_column.button('顔面を塗りつぶす')
-if button:
-    image = Image.open(uploaded_file)
-    image.save("uploaded.jpg")
-    img_array = np.array(image)
-    st.image(img_array,caption = '元画像', use_column_width = True)
+if uploaded_file is None :
+    st.write('顔面が選択されていません')
 
-    face_cascade_path = "haarcascade_frontalface_default.xml"
-    face_cascade = cv2.CascadeClassifier(face_cascade_path)
+else:
+    left_column, right_column = st.columns(2)
+    button = left_column.button('顔面を塗りつぶす')
+    if button:
+        image = Image.open(uploaded_file)
+        image.save("uploaded.jpg")
+        img_array = np.array(image)
+        st.image(img_array,caption = '元画像', use_column_width = True)
 
-    src = cv2.imread('uploaded.jpg')
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) #グレースケール化
-    faces = face_cascade.detectMultiScale(src_gray)
+        face_cascade_path = "haarcascade_frontalface_default.xml"
+        face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
-    for (x, y, w, h) in faces:
-        src[y: y + h, x: x + w] = [0, 128, 255]
+        src = cv2.imread('uploaded.jpg')
+        src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) #グレースケール化
+        faces = face_cascade.detectMultiScale(src_gray)
+
+        for (x, y, w, h) in faces:
+            src[y: y + h, x: x + w] = [0, 128, 255]
     
-    cv2.imwrite('opencv_mosaic_face.jpg', src)
+        cv2.imwrite('opencv_mosaic_face.jpg', src)
 
-    src2 = Image.open('opencv_mosaic_face.jpg')
-    st.image(src2, caption='出力結果', use_column_width = True)
+        src2 = Image.open('opencv_mosaic_face.jpg')
+        st.image(src2, caption='出力結果', use_column_width = True)
